9f188637551dc17ada0531d93937bd2e41038e7d
arvincsh <63835623+arvincsh@users.noreply.github.com>
2020-10-06 15:29:20 +0800

    Update ffmpeg-helper.js

--- ffmpeg-helper.js
+++ ffmpeg-helper.js
@@ -8,11 +8,11 @@ module.exports = {
 //                '-level 1.0',
                 //'-s 640x360',          // 640px width, 360px height
                 '-start_number 0',     // start the first .ts segment at index 0
-                '-hls_list_size 1',    // Maxmimum number of playlist entries)
+                '-hls_list_size 10',    // Maxmimum number of playlist entries)
                 '-f hls',               // HLS format
-                '-hls_wrap 3',
+                '-hls_wrap 10',
                 '-g 30',
-                '-hls_time 2',
+                '-hls_time 0.1',
             ]).output('./hls/nctu.m3u8').on('progress', function(stderr) {
     console.log(stderr);
   }).run()
